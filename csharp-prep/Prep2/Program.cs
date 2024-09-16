38df7f08cec0eafd9c3ed0fe7f761576b1c2c4f7
Adam Clark <adam.b.clark@icloud.com>
2024-09-16 17:51:17 -0600

    Assignment Completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Grade:");
+        String input = Console.ReadLine();
+
+        int num = int.Parse(input);
+        int sign = (num % 10);
+        String signResult = "";
+        String result = "";
+
+        if (sign >= 7) 
+        {
+            signResult = "+";
+        }
+        else if (sign < 3)
+        {
+            signResult = "-";
+        }
+        
+
+        if (num >= 90)
+        {
+            if (signResult.Equals("+"))
+            {
+                result = "n A";
+            }
+            else
+            {
+                result = $"n A{signResult}";
+            }
+           
+        }
+        else if (num >= 80)
+        {
+            result = $" B{signResult}";
+        }
+        else if (num >= 70)
+        {
+            result = $" C{signResult}";
+        }
+        else if (num >= 60)
+        {
+            result = $" D{signResult}";
+        }
+        else {
+            result = $" F";
+        }
+        Console.WriteLine($"You got a{result}.");
     }
 }
