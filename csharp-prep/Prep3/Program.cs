204b213f44549bbf7ec3709ded47bb88425997dc
Adam Clark <adam.b.clark@icloud.com>
2024-09-18 14:41:30 -0600

    Finished Homework Part 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int tryAgain = 0;
+        do
+        {
+            int x = 1;
+            Random ranGen = new Random();
+            int magicNum = ranGen.Next(1,101);
+            Console.WriteLine("What's the magic number? " + magicNum);
+
+            do
+            {
+                Console.WriteLine("What is your guess?");
+                int guess = int.Parse(Console.ReadLine());
+                
+                if (guess == magicNum)
+                {
+                    Console.WriteLine($"You got it in {x}!");
+                    break;
+                }
+                else if (guess < magicNum)
+                {
+                    Console.WriteLine("Under");
+                }
+                else
+                {
+                    Console.WriteLine("Over");
+                }
+                x++;
+            } while( x < 1000);
+            Console.WriteLine("Would you like to try again? (Press 1 to do so)");
+            tryAgain = int.Parse(Console.ReadLine());
+        } while (tryAgain == 1);
     }
 }
