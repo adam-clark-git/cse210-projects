be2a7695a8ce8a050a4f85b877aef01cdceeac4e
Adam Clark <adam.b.clark@icloud.com>
2024-09-23 15:16:09 -0600

    Finished C# Prep 4.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int input = 1;
+        List<int> numList;
+        numList = new List<int>();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.Write("Enter Number:");
+            input = int.Parse(Console.ReadLine());
+            if (input == 0)
+            {
+                break;
+            }
+            numList.Add(input);
+        }
+        int total = 0;
+        int length = numList.Count;
+        int largest = int.MinValue;
+        foreach (int num in numList)
+        {
+            total += num;
+            if (num > largest)
+            {
+                largest = num;    
+            }
+        }
+        int avg = total/ length;
+        Console.WriteLine("The total of the numbers was: " + total);
+        Console.WriteLine("The average number was: " + avg);
+        Console.WriteLine("The largest number was: " + largest);
+        
+
     }
 }
