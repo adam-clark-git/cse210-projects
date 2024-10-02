053cb2a7a510dc340866eb63aeb7fa5f1adbc5fd
Adam Clark <adam.b.clark@icloud.com>
2024-10-02 15:18:44 -0600

    fixed the goofing off of previous adam

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,14 +1,10 @@
 using System;
-// Make a blackjack program
-// One: Array with all cards
-// Two: Be able to remove cards from deck, and add them to owners hand.
-
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("This is a blackjack program");
+        Console.WriteLine("I will die of boredom here.");
         Console.WriteLine("AAAAAAAAAAHHHHHHH");
     }
     
