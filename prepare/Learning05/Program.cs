135b259f4e48f092bcdf1f269d57c15d8b5f519c
adam-clark-git <adam.b.clark@icloud.com>
2024-11-11 14:30:26 -0700

    Implemented Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = [new Circle(2, "blue"), new Rectangle(5, 10, "red"), new Square(3, "purple"), new Shape("green")];
+        foreach (Shape currentShape in shapeList)
+        {
+            Console.WriteLine(currentShape.GetColor());
+            Console.WriteLine(currentShape.GetArea() + "\n");
+        }
+
     }
 }
---
+++ prepare/Learning05/Rectangle.cs
@@ -0,0 +1,14 @@
+class Rectangle : Shape
+{
+    public double _length;
+    public double _width;
+    public Rectangle(double length, double width, string color) : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
